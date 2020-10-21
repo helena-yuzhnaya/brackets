2742f75c6d95a4cbd212a54dbfcf47424f66ac89
helena-yuzhnaya <40338865+helena-yuzhnaya@users.noreply.github.com>
2020-10-21 18:53:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 != 0) {
+    return false;
+  }
+  let arrayOfStringConfig = [];
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    arrayOfStringConfig.push(bracketsConfig[i].join(''))
+  }
+  let attempt = 1;
+  while (attempt) {
+    for (let j = 0; j < arrayOfStringConfig.length; j++) {
+      if (str.includes(arrayOfStringConfig[j])) {
+        str = str.replace(arrayOfStringConfig[j], '');
+          attempt++;
+      }
+    }
+      attempt--;
+  }
+  return str.length == 0;
 }
